b0c25724cfd5da3325a79b306ed0e7678670a8b0
Praveen Muthu <praveen.mut@gmail.com>
2022-07-19 01:07:18 +1000

    Hack out a log(mn) solution.

--- search-2d-matrix.py
+++ search-2d-matrix.py
@@ -2,9 +2,10 @@
 # Squashing time Complexity = O(n)
 # Binary Search time complexity = O(log(n))
 # Total Time complexity = O(log(n)) + O(n) => Ω(n)
-from typing import List
+from typing import List, Optional
 from collections import namedtuple
 import unittest
+from xml.dom import NotFoundErr
 
 def flatten(ary_of_ary):
     flattened_list = []
@@ -28,9 +29,45 @@ def bin_search(ary, target):
       upper = pivot - 1
   return False
 
-class Solution:
+
+class LinearSolution:
   def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
     return bin_search(flatten(matrix),target)
+
+class LogarithmicSolution:
+  _matrix: Optional[list] = None
+  _target: Optional[int] = None
+
+  '''
+  _Synthentically_ search through the 2D matrix
+  as if it were a 1D array.
+  '''
+  def synthetic_bin_search(self, lower: int, upper: int):
+    if lower > upper:
+      return False
+
+    mid = (lower + upper) // 2
+    elements_length = len(self._matrix[0])
+    row, column = divmod(mid,elements_length)
+    current_value = self._matrix[row][column]
+
+    if self._target == current_value:
+      return True
+
+    if self._target < current_value:
+      return self.synthetic_bin_search(lower, mid - 1)
+    elif self._target > current_value:
+      return self.synthetic_bin_search(mid + 1, upper)
+
+    raise RuntimeError("Value was not found or an unexpected exception has occured")
+
+  def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+    self._matrix = matrix
+    self._target = target
+    total_elements = (len(matrix[0]) * len(matrix)) - 1
+    if target > matrix[-1][-1] or target < matrix[0][0]: return False
+    self.synthetic_bin_search(0,  total_elements)
+
 
 TestCase = namedtuple("TestCase", "case target")
 
@@ -44,9 +81,11 @@ case2 = TestCase([[1,3,5,7],[10,11,16,20],[23,30,34,60]],13)
 
 class TestSolution(unittest.TestCase):
     def test_happy(self):
-        self.assertEqual(Solution().searchMatrix(matrix=case1.case,target=case1.target),True)
+        self.assertEqual(LinearSolution().searchMatrix(matrix=case1.case,target=case1.target),True)
+        self.assertEqual(LogarithmicSolution().searchMatrix(matrix=case1.case,target=case1.target),True)
     def test_sad(self):
-        self.assertEqual(Solution().searchMatrix(matrix=case2.case,target=case2.target),False)
+        self.assertEqual(LinearSolution().searchMatrix(matrix=case2.case,target=case2.target),False)
+        self.assertEqual(LogarithmicSolution().searchMatrix(matrix=case2.case,target=case2.target),False)
 
 
 if __name__ == '__main__':
